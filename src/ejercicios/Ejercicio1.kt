cf5a66f38b9729868a145f566d9bf63b4481d5ba
alu10216804 <alubar804@ieselcaminas.org>
2021-10-08 18:21:59 +0200

    ejer1

---
+++ src/ejercicios/Ejercicio1.kt
@@ -0,0 +1,24 @@
+package ejercicios
+
+import java.io.DataInputStream
+import java.io.FileInputStream
+import java.io.RandomAccessFile
+import java.util.*
+
+
+    fun main(args: Array<String>){
+        val f = DataInputStream(FileInputStream("Rutes.dat"))
+
+        while (f.available() > 0) {
+            System.out.println("Ruta: " + f.readUTF())
+            System.out.println("Desnivell: " + f.readInt())
+            System.out.println("Desnivell acumulat: " + f.readInt())
+            val punts = f.readInt()
+            System.out.println("Te ${punts} punts")
+            for (i in 1..punts){
+                println("Punt ${i}: ${f.readUTF()} (${f.readDouble()} ${f.readDouble()})")
+            }
+            println("")
+        }
+        f.close()
+}
