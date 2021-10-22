c306c9bbdcb417bd30001a6b7b740c6c20b12e88
alu10216804 <alubar804@ieselcaminas.org>
2021-10-22 19:33:58 +0200

    fin

--- src/ejercicios/EX6.kt
+++ src/ejercicios/EX6.kt
@@ -1,16 +1,11 @@
 package ejercicios
 
-class EX6 {
-}
+
 import javax.swing.*
 import java.awt.*
-import com.squareup.moshi.Moshi
-import exercicis.Ruta
-import org.json.JSONArray
-import org.json.JSONObject
+
+import org.json.*
 import org.json.JSONTokener
-import org.w3c.dom.Element
-import java.io.File
 import java.io.FileReader
 
 class FinestraJSON : JFrame() {
@@ -18,8 +13,8 @@ class FinestraJSON : JFrame() {
     init {
         var llistaRutes: ArrayList<Ruta>
         // sentències per a omplir llistaRutes
-        val r_json = FileReader("Rutas.json")
-        val arrel = JSONTokener(r_json).nextValue() as JSONObject
+        val f = FileReader("Rutes.json")
+        val raiz = JSONTokener(f).nextValue() as JSONObject
 
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
         setTitle("JSON: Punts d'una ruta")
@@ -31,14 +26,14 @@ class FinestraJSON : JFrame() {
         add(panell1, BorderLayout.NORTH)
         add(panell2, BorderLayout.CENTER)
 
-        var nomsLlistaRutes = arrayListOf<String>()
+        var listaNombres = arrayListOf<String>()
         // sentències per a omplir l'ArrayList anterior amb el nom de les rutes
-        for (i in arrel.getJSONArray("rutes")) {
-            val jo = i as JSONObject
-            nomsLlistaRutes.add(jo.get("nom") as String)
+        for (i in raiz.getJSONArray("rutes")) {
+            val e = i as JSONObject
+            listaNombres.add(e.get("nom") as String)
         }
 
-        val combo = JComboBox(nomsLlistaRutes.toArray())
+        val combo = JComboBox(listaNombres.toArray())
         panell1.add(combo)
 
         panell2.add(JLabel("Llista de punts de la ruta:"), BorderLayout.NORTH)
@@ -48,19 +43,19 @@ class FinestraJSON : JFrame() {
         combo.addActionListener {
             // accions quan s'ha seleccionat un element del combobox,
             // i que han de consistir en omplir el JTextArea
-            val arr = arrel.getJSONArray("rutes")
-            val ruta = arr[combo.selectedIndex] as JSONObject
+            val elarray = raiz.getJSONArray("rutes")
+            val ruta = elarray[combo.selectedIndex] as JSONObject
             val punts = ruta.getJSONArray("punts") as JSONArray
-            var acum = ""
+            var palabras = ""
             for (i in punts) {
-                val a = i as JSONObject
-                val nom = a.get("nom")
-                val coor = a.getJSONObject("coor")
-                val latitud = coor.get("latitud")
-                val longitud = coor.get("longitud")
-                acum += "${nom} (${latitud} ${longitud})\n"
+                val e = i as JSONObject
+                val nom = e.get("nom")
+                val coord = e.getJSONObject("coor")
+                val latitud = coord.get("latitud")
+                val longitud = coord.get("longitud")
+                palabras += "${nom} (${latitud} ${longitud})\n"
             }
-            area.text = acum
+            area.text = palabras
         }
     }
 }
@@ -69,50 +64,4 @@ fun main(args: Array<String>) {
     EventQueue.invokeLater {
         FinestraJSON().isVisible = true
     }
-}
-import exercicis.Ruta
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.EOFException
-import java.io.FileInputStream
-import java.io.FileWriter
-
-import java.io.ObjectInputStream
-
-fun main() {
-    val f = ObjectInputStream(FileInputStream("Rutas.obj"))
-
-    val arrel = JSONObject()
-    val rutesArray = JSONArray()
-    arrel.put("rutes", rutesArray)
-    try {
-        while (true) {
-            val obj = f.readObject() as Ruta
-
-            val ruta = JSONObject()
-            val punts = JSONArray()
-
-            ruta.put("nom", obj.nom)
-            ruta.put("desnivell", obj.desnivell)
-            ruta.put("desnivellAcumulat", obj.desnivellAcumulat)
-
-            for (i in obj.llistaDePunts) {
-                val punt = JSONObject()
-                val coor = JSONObject()
-                punt.put("nom", i.nom)
-                coor.put("latitud", i.coord.latitud)
-                coor.put("longitud", i.coord.longitud)
-                punt.put("coor", coor)
-                punts.put(punt)
-            }
-            ruta.put("punts", punts)
-            rutesArray.put(ruta)
-        }
-    } catch (eof: EOFException) {
-        f.close();
-        val e = FileWriter("Rutas.json")
-        e.write(arrel.toString(4))
-        e.close()
-    }
-
 }
--- src/ejercicios/Ex3_5_PassarRutesObjJSON.kt
+++ src/ejercicios/Ex3_5_PassarRutesObjJSON.kt
@@ -1,66 +1,52 @@
 package ejercicios
 
-import org.json.JSONObject
+
 import org.json.JSONArray
+import org.json.JSONObject
 import java.io.EOFException
 import java.io.FileInputStream
 import java.io.FileWriter
+
 import java.io.ObjectInputStream
 
-fun main(args: Array<String>) {
-    val inp = ObjectInputStream(FileInputStream("Rutes.obj"))
-    val f = FileWriter("Rutes.json")
-//    var noms = mutableListOf<String>()
-//    var desnivells = mutableListOf<Int>()
-//    var desnivellAcumulats = mutableListOf<Int>()
-//    var  = mutableListOf<PuntGeo>()
-//    var  = mutableListOf<PuntGeo>()
 
-    val raiz = JSONObject()
-    val rutas = JSONArray()
-    raiz.put("rutes",rutas)
+fun main() {
+    val f = ObjectInputStream(FileInputStream("Rutes.obj"))
+
+    val arrel = JSONObject()
+    val rutesArray = JSONArray()
+    arrel.put("rutes", rutesArray)
     try {
-
-
-        var x = 0
         while (true) {
+            val obj = f.readObject() as Ruta
+
             val ruta = JSONObject()
-            val noms = JSONObject()
-            val desnivells = JSONObject()
-            val desnivellAcumulats = JSONObject()
-            val carac = JSONArray()
-            val e = inp.readObject() as Ruta
-            noms.put("nom",e.nom)
-            desnivells.put("desnivell",e.desnivell)
-            desnivellAcumulats.put("desnivell acumulat",e.desnivellAcumulat)
-            carac.put(noms)
-            carac.put(desnivells)
-            carac.put(desnivellAcumulats)
-            for (i in 0 until e.size()){
+            val punts = JSONArray()
+
+            ruta.put("nom", obj.nom)
+            ruta.put("desnivell", obj.desnivell)
+            ruta.put("desnivellAcumulat", obj.desnivellAcumulat)
+
+            for (i in obj.llistaDePunts) {
                 val punt = JSONObject()
-                punt.put("nom",e.getPuntNom(i).toString())
-                val coord = JSONObject()
-                coord.put("latitud",e.getPuntLatitud(i).toString())
-                coord.put("longitud",e.getPuntLongitud(i).toString())
-                punt.put("coord",coord)
-                carac.put(punt)
-
+                val coor = JSONObject()
+                punt.put("nom", i.nom)
+                coor.put("latitud", i.coord.latitud)
+                coor.put("longitud", i.coord.longitud)
+                punt.put("coord", coor)
+                punts.put(punt)
             }
-            ruta.put("ruta",carac)
-            rutas.put(ruta)
 
-
-            x++
-
+            ruta.put("punts", punts)
+            rutesArray.put(ruta)
         }
-
     } catch (eof: EOFException) {
-        inp.close()
+        f.close();
+        val e = FileWriter("Rutes.json")
+        e.write(arrel.toString(4))
+        e.close()
     }
-    f.write(raiz.toString(4))
 
-
-    inp.close()
-    f.close()
 }
+
 
